8b6e6b163858bbbcb1ae0c6579415feec84aa500
1988Igor <110403993+1988Igor@users.noreply.github.com>
2023-03-28 08:28:44 +0200

    redacted the comment

--- hw5Task2.sql
+++ hw5Task2.sql
@@ -30,12 +30,15 @@ JOIN (
          LEAD(station_time) OVER (PARTITION BY trains_id ORDER BY station_time) AS next_station_time
   FROM train_station
 ) t2 ON t1.trains_id = t2.trains_id AND t1.station = t2.station AND t1.station_time = t2.station_time
-SET t1.time_to_next_station = COALESCE(TIMESTAMPDIFF(MINUTE, t1.station_time, t2.next_station_time), 0); -- Если следующей станции нет, то мы присваиваем значение 0 столбцу "time_to_next_station" с помощью функции COALESCE().
+SET t1.time_to_next_station = COALESCE(TIMESTAMPDIFF(MINUTE, t1.station_time, t2.next_station_time), 0); 
+-- Если следующей станции нет, то мы присваиваем значение 0 столбцу "time_to_next_station" с помощью функции COALESCE().
 
 SELECT trains_id, station,  station_time,
- SEC_TO_TIME(time_to_next_station * 60) AS time_to_next_station -- выбираем все строки из таблицы "train_station" и конвертируем значения столбца "time_to_next_station" в формат времени с помощью функции SEC_TO_TIME().
+ SEC_TO_TIME(time_to_next_station * 60) AS time_to_next_station   
 FROM train_station;
-
+/*выбираем все строки из таблицы "train_station" и конвертируем значения столбца 
+"time_to_next_station" в формат времени с помощью функции SEC_TO_TIME().
+*/
 
 
 
